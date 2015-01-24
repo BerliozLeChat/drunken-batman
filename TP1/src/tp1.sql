55a45d8a19e3366fb7fe0672efb7975f824c0cd5
Giraudux <giraudux@gmail.com>
2015-01-24 19:28:28 +0100

    fix #1

--- TP1/src/tp1.sql
+++ TP1/src/tp1.sql
@@ -5,116 +5,93 @@ Licence 3 informatique
 Université de Nantes
 */
 
---Destruction des tables 
-drop table ACHATS;
-drop table AVIS;
-drop table LIVRES;
-drop table CLIENTS;
 
 
---Création des tables
-create table CLIENTS(
-  idcl integer primary key not null,
-  nom varchar2(20),
-  pren varchar2(15),
-  adr varchar2(30),
-  tel varchar2(12)
-);
+-- Déclaration
+create table Clients ( idcl number       not null,
+                       nom  varchar2(20) not null,
+                       pren varchar2(15) not null,
+                       adr  varchar2(30) not null,
+                       tel  varchar2(12) not null,
+                     primary key (idcl) );
 
-create table LIVRES(
-  refl varchar2(10) primary key not null,
-  titre varchar2(50), 
-  auteur varchar2(20), 
-  genre varchar2(15)
-);
+create table Livres ( refl   varchar2(10) not null,
+                      titre  varchar2(20) not null,
+                      auteur varchar2(20) not null,
+                      genre  varchar2(15) not null,
+                    primary key (refl) );
 
-create table ACHATS(
-  idcl integer not null, 
-  refl varchar2(10), 
-  dateachat date primary key not null
-);
+create table Achats ( idcl      number       not null,
+                      refl      varchar2(10) not null,
+                      dateachat date         not null,
+                    primary key (idcl, refl, dateachat),
+                    foreign key (idcl) references Clients,
+                    foreign key (refl) references Livres );
 
-create table AVIS(
-  idcl integer not null, 
-  refl varchar2(10), 
-  note number(4,2), 
-  commentaire varchar2(50)
-);
+create table Avis ( idcl number       not null,
+                    refl varchar2(10) not null,
+                    note number(4,2)  not null,
+                    commentaire varchar2(50),
+                  primary key (idcl, refl),
+                  foreign key (idcl) references Achats,
+                  foreign key (refl) references Achats );
 
---Ajout des clés étrangères
-alter table AVIS 
-add foreign key (idcl)
-references CLIENTS(idcl);
+-- Insertion
+insert into Clients values (0, 'nom0', 'pren0', 'adr0', 'tel0');
+insert into Clients values (1, 'nom1', 'pren1', 'adr1', 'tel1');
+insert into Clients values (2, 'nom2', 'pren2', 'adr2', 'tel2');
+insert into Clients values (3, 'nom3', 'pren3', 'adr3', 'tel3');
+insert into Clients values (4, 'nom4', 'pren4', 'adr4', 'tel4');
+insert into Clients values (5, 'nom5', 'pren5', 'adr5', 'tel5');
+insert into Clients values (6, 'nom6', 'pren6', 'adr6', 'tel6');
+insert into Clients values (7, 'nom7', 'pren7', 'adr7', 'tel7');
+insert into Clients values (8, 'nom8', 'pren8', 'adr8', 'tel8');
+insert into Clients values (9, 'nom9', 'pren9', 'adr9', 'tel9');
 
-alter table ACHATS 
-add foreign key (idcl)
-references CLIENTS(idcl);
+insert into Livres values ('refl0', 'titre0', 'auteur0', 'genre0');
+insert into Livres values ('refl1', 'titre1', 'auteur1', 'genre1');
+insert into Livres values ('refl2', 'titre2', 'auteur2', 'genre2');
+insert into Livres values ('refl3', 'titre3', 'auteur3', 'genre3');
+insert into Livres values ('refl4', 'titre4', 'auteur4', 'genre4');
+insert into Livres values ('refl5', 'titre5', 'auteur5', 'genre5');
+insert into Livres values ('refl6', 'titre6', 'auteur6', 'genre6');
+insert into Livres values ('refl7', 'titre7', 'auteur7', 'genre7');
+insert into Livres values ('refl8', 'titre8', 'auteur8', 'genre8');
+insert into Livres values ('refl9', 'titre9', 'auteur9', 'genre9');
 
-alter table ACHATS 
-add foreign key (refl)
-references LIVRES(refl);
+insert into Achats values (0, 'refl0', current_date);
+insert into Achats values (0, 'refl1', current_date);
+insert into Achats values (0, 'refl2', current_date);
+insert into Achats values (0, 'refl3', current_date);
+insert into Achats values (0, 'refl4', current_date);
+insert into Achats values (1, 'refl5', current_date);
+insert into Achats values (1, 'refl6', current_date);
+insert into Achats values (1, 'refl7', current_date);
+insert into Achats values (1, 'refl8', current_date);
+insert into Achats values (1, 'refl9', current_date);
 
--- insertions
+insert into Avis values (0, 'refl0' ,15 , null);
+insert into Avis values (0, 'refl1' ,16 , 'commentaire1');
+insert into Avis values (0, 'refl2' ,17 , null);
+insert into Avis values (0, 'refl3' ,18 , 'commentaire3');
+insert into Avis values (0, 'refl4' ,10 , null);
+--insert into Avis values (0, 'refl9' ,15 , null); -- Erreur: Livre non acheté
 
---Table Clients
-insert into CLIENTS
-values (1,'dupont','jean','somewhere',66666666);
+-- Extractcion
+-- 1. Les meilleures ventes : les livres (titre, auteur, genre) qui ont été achetés en plus de 10000 exemplaires.
+select titre, auteur, genre
+from Livres livre
+where ( select count(refl)
+        from Achats
+        where refl = livre.refl ) > 10000;
 
-insert into CLIENTS
-values (2,'dupond','michel','somewhere',66666666);
+--2. Les livres qui obtiennent une note moyenne supérieure à 16.
+select *
+from Livres natural join Avis
+group by refl, note
+having sum(note)/count(refl) > 16;
 
-insert into CLIENTS
-values (3,'blop','jacques','somewhere',66666666);
-
-insert into CLIENTS
-values (4,'blup','dédé','somewhere',66666666);
-
---Table Livres
-insert into LIVRES
-values ('4589632587','Fifty Shades of grey','E.L.James','Erotique');
-
-insert into LIVRES
-values ('4589632588','Fifty Shades Darker','E.L.James','Erotique');
-
-insert into LIVRES
-values ('4589632589','Fifty Shades Freed','E.L.James','Erotique');
-
---Table Achats
-insert into ACHATS
-values (2,'4589632587','14-02-2009');
-
-insert into ACHATS
-values (2,'4589632588','15-02-2009');
-
-insert into ACHATS
-values (2,'4589632589','16-02-2009');
-
-insert into ACHATS
-values (4,'4589632589','16-12-2009');
-
---Table Avis
-insert into AVIS
-values (2,'4589632588',99.99,'Cool, cool cool cool');
-
-insert into AVIS
-values (4,'4589632588',18,null);
-
-
---------Les requêtes---------------------------
-
---1/ Les meilleures ventes : les livres (titre, auteur, genre) qui ont été achetés
--- en plus de 10000 exemplaires.
- select titre, auteur, genre from livres where 
- (select count(refl) from achats where refl = livres.refl)>=10000;
----------------------------------------------------------------------------------
-
---2/Les livres qui obtiennent une note moyenne supérieure à 16.
- select titre, auteur, genre from livres natural join avis
-group by refl having sum(note)/(select count(refl) from avis)>=16;
----------------------------------------------------------------------------------
-
---3/Les clients qui n'ont pas renseigné l'attribut commentaire d'un livre
--- (quel client, livre, note).
-select nom, pren, titre, note from clients,livres, avis where
-commentaire = null;
- 
+--3. Les clients qui n'ont pas renseigné l'attribut commentaire d'un livre (quel client, livre, note).
+select *
+from Clients natural join Avis
+where commentaire is null;
