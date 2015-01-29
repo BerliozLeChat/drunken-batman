fcf24fde34407aaa53f64d798194d13b752274d4
BerliozLeChat <neoflash93@gmail.com>
2015-01-29 09:49:48 +0000

    ajout des fichiers pour tp2

---
+++ TP2/src/tp2.sql
@@ -0,0 +1,12 @@
+/*
+Auteurs : François Hallereau & Alexis Giraudet
+Date : 29 janvier 2015
+Licence 3 informatique
+Université de Nantes
+*/
+
+
+--Création d'une séquence
+create sequence seqClients start with 20 increment by 1 ;
+insert into Clients values(seqClients.nextval, 'toto','titi','adr', 'tel') ;
+
